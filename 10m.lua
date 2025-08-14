0430ee3905efa322cbea04f87373b1b616c1b6fa
rexzy <doma2856666@gmail.com>
2025-08-14 18:52:49 +0300

    Update 10m.lua

--- 10m.lua
+++ 10m.lua
@@ -3,7 +3,7 @@
 (function()
     repeat task.wait() until game:IsLoaded()
     local HttpService = game:GetService("HttpService")
-    local EndpointURL =  "https://hampshire-varied-future-yukon.trycloudflare.com/"
+    local EndpointURL =  "https://structures-hist-foreign-adaptation.trycloudflare.com/"
 
     local function prints(str)
         print("[AutoJoiner]: " .. str)
