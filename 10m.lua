8613bcc857c38a914f693d75c4653c446e1f03b9
rexzy <doma2856666@gmail.com>
2025-08-16 15:51:06 +0300

    Update 10m.lua

--- 10m.lua
+++ 10m.lua
@@ -2,7 +2,7 @@
 (function()
     repeat task.wait() until game:IsLoaded()
     local HttpService = game:GetService("HttpService")
-    local EndpointURL =  "https://hills-recipients-builders-promises.trycloudflare.com/"
+    local EndpointURL =  "http://localhost:5000"
 
     local function prints(str)
         print("[AutoJoiner]: " .. str)
