4ddb6ac84a18080aa2ada05fa6f1767ef48d6106
rexzy <doma2856666@gmail.com>
2025-08-16 22:36:27 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -90,7 +90,7 @@ local function createButton(text, order, scriptEndpoint)
 end
 
 -- Create buttons with different script endpoints
-createButton("Start auto join", 1, "https://stripes-pst-doom-inline.trycloudflare.com/script")
-createButton("1-3M", 2, "https://stripes-pst-doom-inline.trycloudflare.com/1-3m-script")
-createButton("3-6M", 3, "https://stripes-pst-doom-inline.trycloudflare.com/3-6m-script")
-createButton("6-9M", 4, "https://stripes-pst-doom-inline.trycloudflare.com/6-9m-script")
+createButton("Start auto join", 1, "https://checkout-angeles-quantum-interval.trycloudflare.com/script")
+createButton("1-3M", 2, "https://checkout-angeles-quantum-interval.trycloudflare.com/1-3m-script")
+createButton("3-6M", 3, "https://checkout-angeles-quantum-interval.trycloudflare.com/3-6m-script")
+createButton("6-9M", 4, "https://checkout-angeles-quantum-interval.trycloudflare.com/6-9m-script")
